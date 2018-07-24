46f1ef16acb89680facca46a68f93114a3ba1148
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-24 23:29:51 +0100

    Added filtering system (#3)
    * added output
    
    * fix layout
    
    * travis
    
    * remove dep

--- src/filter.jl
+++ src/filter.jl
@@ -1,4 +1,4 @@
-@widget wdg function addfilter(t)
+@widget wdg function addfilter(t; readout = true)
     t isa Observable || (t = Observable{Any}(t))
     :cols = dropdown(map(colnames, t), label = "Column to filter")
     selectoptions = OrderedDict(
@@ -8,18 +8,30 @@
     )
     :selectortype = dropdown(selectoptions, label = "Selector type")
     :button = button("Add selector")
+    :filter = button("Filter")
     function columnlayout(v)
         cols = map(Widgets.div(className = "column"), v)
-        Widgets.div(className = "columns is-multiline", cols...)
+        Widgets.div(className = "columns is-multiline is-mobile", cols...)
     end
     :selectors = deletablelist([], layout = columnlayout)
+
+    lazymap(f, v) = (f(i) for i in v)
     @on wdg begin
         $(:button)
-        push!(:selectors[], :selectortype[](t[], :cols[]))
+        push!(:selectors[], :selectortype[](t[], :cols[], lazymap))
         :selectors[] = :selectors[]
     end
+
+    @output! wdg Observable{Any}(t[])
+
+    on(observe(wdg[:filter])) do x
+        sels = (observe(i)[] for i in observe(wdg[:selectors])[])
+        wdg.output[] = t[][[all(i) for i in zip(sels...)]]
+    end
+
     @layout! wdg Widgets.div(
-        Widgets.div(className = "level is-mobile", map(Widgets.div(className="level-item"), [:cols, :selectortype, :button])...),
+        Widgets.div(className = "level is-mobile", map(Widgets.div(className="level-item"), [:cols, :selectortype, :button, :filter])...),
         :selectors,
+        toggletable(_.output, readout = true)
     )
 end
