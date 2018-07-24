899772ec5c487c23b12b8b006043ad6fa1c573a7
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-24 17:34:32 +0100

    hack before webio supports node

--- src/TableWidgets.jl
+++ src/TableWidgets.jl
@@ -3,12 +3,15 @@ module TableWidgets
 using InteractBase, InteractBulma, Widgets, CSSUtil, Observables, JuliaDBMeta, IndexedTables, WebIO
 using FileIO, IterableTables
 using Compat
-import Widgets: node
 import DataStructures: reset!
 
 using MacroTools
 
 export categoricalselector, rangeselector, selector
+
+# hack before rdeits PR is merged in webio
+node(args...; kwargs...) = Node(args...; kwargs...)
+node(s::AbstractString, args...; kwargs...) = node(Symbol(s), args...; kwargs...)
 
 include("utils.jl")
 include("selector.jl")
