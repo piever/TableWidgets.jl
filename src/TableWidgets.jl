6079ca12b677e74b135d55f8708e0569adef4180
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-09-02 13:47:20 +0100

    stop importing non updated packages

--- src/TableWidgets.jl
+++ src/TableWidgets.jl
@@ -1,7 +1,8 @@
 module TableWidgets
 
-using InteractBase, Widgets, CSSUtil, Observables, JuliaDBMeta, IndexedTables, WebIO
-import IndexedTables: AbstractIndexedTable
+using InteractBase, Widgets, CSSUtil, Observables, WebIO
+# using JuliaDBMeta, IndexedTables
+# import IndexedTables: AbstractIndexedTable
 import InteractBulma
 using Compat
 using DataStructures
@@ -10,12 +11,6 @@ import DataStructures: reset!
 using MacroTools
 
 export categoricalselector, rangeselector, selector
-
-# hack before rdeits PR is merged in webio
-@static if !isdefined(WebIO, :node)
-    node(args...; kwargs...) = Node(args...; kwargs...)
-    node(s::AbstractString, args...; kwargs...) = node(Symbol(s), args...; kwargs...)
-end
 
 include("utils.jl")
 # include("selector.jl")
